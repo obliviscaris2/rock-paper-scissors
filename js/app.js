2df52d67494e9f9682340412655b2eacd94a7289
@neobliviscaris2 <ergasiamellow@gmail.com>
2022-05-01 14:25:59 +0545

    trial

--- js/app.js
+++ js/app.js
@@ -1,61 +1,85 @@
+const container = document.querySelector('#container');
 
-const rockButton = document.querySelector('#rock');
-const paperButton = document.querySelector('#paper');
-const scissorsButton = document.querySelector('#scissors');
-const buttons = document.querySelectorAll('#btn');
+const gamePlay = document.querySelector('#play');
+const turns = document.querySelector('.turns-left');
+const playerButtons = document.querySelectorAll('#btn');
+playerButtons.forEach(button => button.disabled = true); // Disabling buttons at the start of the game
+const gameReset = document.querySelector('#refresh');
+
+const displayPlayerMove = document.querySelector('.player-move');
+const displayComputerMove = document.querySelector('.computer-move');
+const playerTally = document.querySelector('.player-tally');
+const computerTally = document.querySelector('.computer-tally');
 const result = document.querySelector('.result');
-const PLAYER_SELECT = document.querySelector('.playerAnswer');
 
+let computerSelect;
+let turnsLeft = 5;
 
+let gameState = 0;
 
-let computerChoice = computerMove();
-let playerChoice = playerMove()
+// Making play button work
 
-let gameState = 1;
+gamePlay.addEventListener('click', (e) => {
+    gamePlay.disabled = true;
+    gameState = 1;
+    if (gameState = 1) {
+        playerButtons.forEach(button => button.disabled = false);
+        playerButtons.forEach(playerButton => {
+            playerButton.addEventListener('click', (e) => {
+                if (turnsLeft > 0) {
+                    turnsLeft -= 1;
+                    turns.innerText = turnsLeft;
+                    let playerMove;
+                    let computerMove = computerPlay();
+                    playerMove = e.currentTarget.value;
+                    console.log(computerMove)
+                    console.log(playerMove)
+                    compareMove(playerMove, computerMove);
+                    displayPlayerMove.innerText = `Your move: ${playerMove}`;
+                    displayComputerMove.innerText = `Computer's move: ${computerMove}`;
+                    result.innerText = `${compareMove(playerMove, computerMove)}`
+                
+                }if (turnsLeft === 0) {
+                    playerButtons.forEach(button => {
+                        button.disabled = true;
+                        gameState = 0;
+                    });
+                    
+                }
+        
+            });
+        });
+        // Function to create computer choices... 
+    
+        function computerPlay(){
+            let computerOptions = ['rock', 'paper', 'scissors'];
+            computerSelect = Math.floor(Math.random() * computerOptions.length)
+            let computerChoice = computerOptions[computerSelect];
+            return computerChoice;
+        };
+    
+    
+    // Function to compare the moves.....
+    
+        function compareMove(playerSelection, computerSelection){
+            let newResult = ''
+            if (playerSelection === computerSelection) {
+                newResult = "It is a draw, dawgg";
+        
+            }else if(playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper') {
+                newResult = "You did it, yousonofabitch";
+        
+            }else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock'){
+                newResult = "You lost, you worthless scum";
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        if (button.id === 'btn') {
-            playRound(playerChoice, computerChoice)
-        }
-    })
+            }
+            return (newResult);
+        };   
+    }  
+});
+
+// Making refresh button's function -- 
+
+gameReset.addEventListener('click', (e) => {
+    window.location.reload();
 })
-
-
-function playerMove() {
-    buttons.forEach(button => {
-        button.addEventListener('click', (e) => {
-            return (e.target.value);
-            })
-    })
-};
-
-
-
-function playRound(playerSelection, computerSelection) {
-    let currentResult = '';
-
-    if(playerSelection === computerSelection) {
-        currentResult += "It's a tie";
-    
-    }
-    if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
-        currentResult += "You lost, dawg";
-    
-    }
-    if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper') {
-        currentResult += "You win dawg"
-    };
-
-    return result.innerText = currentResult;
-
-
-};
-
-
-function computerMove() {
-    let computerOptions = ['rock', 'paper', 'scissors'];
-    computerSelect = Math.floor(Math.random() * computerOptions.length);
-    let computerAnswer = computerOptions[computerSelect];
-    return (computerAnswer);
-};
